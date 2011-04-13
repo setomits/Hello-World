8890b8aa0fab72d5bdd81f433d0eb38f952f70f9
Mitsuhiro Setoguchi <setomits@gmail.com>
2011-04-13 15:20:39 +0900

    First commit of sample code of mod_wsgi with webob module.

---
+++ hello_modwsgi.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from webob import Request, Response
+
+class MyApp(object):
+    def __call__(self, environ, start_response):
+        self.req = Request(environ)
+        self.resp = Response()
+        self.start_response = start_response
+
+        return self._main()
+
+    def _main(self):
+        if self.req.POST:
+            meth = 'POST'
+            var1, var2 = (self.req.POST.get(k, u'') for k in ('var1', 'var2'))
+        else:
+            meth = 'GET'
+            var1, var2 = (self.req.GET.get(k, u'') for k in ('var1', 'var2'))
+        o = u'Method: "%s", var1: "%s", var2: "%s"' % (meth, var1, var2)
+
+        self.resp.body = o.encode('utf-8', 'ignore')
+
+        self.start_response(self.resp.status, self.resp.headerlist)
+        return self.resp.body
+
+application = MyApp()
